3f1f8d99f283fb5defc3f8bee41c7bc533ae5fc7
Kay <wasilkayla@gmail.com>
2023-03-04 11:48:54 -0500

    commit 2-planets v1

--- sketch.js
+++ sketch.js
@@ -1,46 +1,46 @@
 let t = 0;
 let num = 10;
-//let x1, x2, y1, y2;
+let sZ;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(20);
+  //background(20);
 }
 
 function draw() {
- // background(20);
+  background(20);
   stroke(255);
   strokeWeight(5);
-
-
-  // x1= sin(t/10)*100+sin(t/5)*20;
-  // y1 = cos(t/10)*100;
-  // x2 = sin(t/10)*200+sin(t)*2;
-  // y2 = cos(t/20)*200 +cos(t/12)*20;
-
   translate(width/2, height/2);
+  circle(0,0, 300);
+  //draw faint fractal in bg
+  //add some more planet orbits
+  //increment/change central planet somehow- look at generative sketch
 
 for (let i =0; i<num; i++){
   //line(x1(t+i),y1(t+i),x2(t+i),y2(t+i));
-  point(x1(t),y1(t));
-  point(x2(t),y2(t));
+  // point(x1(t),y1(t));
+  // point(x2(t),y2(t));
+  sZ = map(x2(t), 0, width, 25, 100);
+  circle(x1(t),y1(t),sZ);
+  console.log(sZ);
 }
-  t+=0.05;
+  t+=0.1;
 
 }
 
+//big oval loop path
 function x1(t){
-  return sin(t/10) *100 + sin(t/5)*20;
+  return sin(t/10) *(width/2);
 }
-
 function y1(t){
   return cos(t/10) *100;
 }
 
 function x2(t){
-  return sin(t/10)*200+sin(t)*2;
+  return sin(t/10)*(width/3) + sin(t/5)*50;
 }
 
 function y2(t){
-  return cos(t/20) *200 + cos(t/12)*20;
+  return cos(t/20) *(height/3) + cos(t/5)*50;
 }
